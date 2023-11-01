eafb85b2f8440fc4d582976b456b5f9d2fbeed03
Yash Kathe <yashkathe123@gmail.com>
2023-10-31 17:02:56 -0700

    mongodb integrated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
+config();
 const app = express();
 
 // Middlewares
-app.use(express.json())
+app.use(express.json());
 
 // Connections and Listeners
-app.listen(process.env.PORT || 5000, () => console.log(`server started on port ${process.env.PORT || 5000}`));
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@new-cluster.syllbdh.mongodb.net/ai-chat-bot`
+	)
+	.then(() => {
+		app.listen(process.env.PORT || 5000);
+		console.log(`Server started on port ${process.env.PORT || 5000} and Mongo DB is connected`);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
